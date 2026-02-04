d5394aa1f24e2d5eb19c43b430e66c7646d46064
Etta Vani <vanireddy16825@gmail.com>
2026-02-04 17:44:49 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,16 +1,16 @@
 from keras.models import Sequential
 from keras.layers import Conv2D , MaxPooling2D , Flatten , Dense
-# creating model
+# creating CNN model for drowsiness detection
 model = Sequential()
  
 model.add(Conv2D(64 , (3,3) , activation = 'relu' , input_shape= X.shape[1:]))
-model.add(MaxPooling2D((1,1)))
+model.add(MaxPooling2D((2,2)))
  
 model.add(Conv2D(64 , (3,3) , activation = 'relu'))
-model.add(MaxPooling2D((1,1)))
+model.add(MaxPooling2D((2,2)))
  
 model.add(Conv2D(64 , (3,3) , activation = 'relu'))
-model.add(MaxPooling2D((1,1)))
+model.add(MaxPooling2D((2,2)))
  
 model.add(Flatten())
  
